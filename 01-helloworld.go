b16505b422769ec296dcdd1dc1eab8c31f87892a
COLM WOODLOCK - STUDENT <G00341460@gmit.ie>
2017-09-19 14:51:19 +0100

    My first commit!

---
+++ 01-helloworld.go
@@ -0,0 +1,12 @@
+//Hello World in Go
+//Author: Colm Woodlock
+
+//Adapted from https://tour.golang.org/welcome/1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, ??")
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+test
+Chnage to directory
+git init
+git add README.md
