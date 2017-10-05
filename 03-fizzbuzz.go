77fff96dbf998929fd1a395c525231ab9ae31f51
Colm Woodlock <Colm Woodlock>
2017-10-05 18:35:10 +0100

    Added comments

--- 03-fizzbuzz.go
+++ 03-fizzbuzz.go
@@ -1,3 +1,8 @@
+//FizzBuzz exercise in Go
+//Author: Colm Woodlock
+
+//Adapted from http://wiki.c2.com/?FizzBuzzTest
+
 package main
 
 import "fmt"
@@ -6,15 +11,15 @@ import "fmt"
 func main() {
 	i := 1
 	for i < 50 {
-		if i%3 == 0 && i%5 == 0 {
+		if i%3 == 0 && i%5 == 0 { //If statement to check if it is divisible by 3 and 5
 			fmt.Println("FizzBuzz")
-		} else if i%5 == 0 {
+		} else if i%5 == 0 { //If statement to check if it is divisible by only 5
 			fmt.Println("Buzz")
-		} else if i%3 == 0 {
+		} else if i%3 == 0 { //If statement tot check if it is only divisible by 3
 			fmt.Println("Fizz")
 		} else {
 			fmt.Println(i)
 		}
-		i++
+		i++ //increment i
 	}
 }
