10921180588cefb355a417cd15f834bce914438c
Colm Woodlock <Colm Woodlock>
2017-10-05 18:05:32 +0100

    Current time question

---
+++ 02-currenttime.go
@@ -0,0 +1,15 @@
+//Print the current time in Go
+//Author: Colm Woodlock
+
+//Adapted from https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("The time is", time.Now())
+}
