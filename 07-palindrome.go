89c69c9297919c5bed7417c08655964908a2c7b6
cwoodlock <g00341460@gmit.ie>
2017-10-06 23:14:12 +0100

    added comments

--- 07-palindrome.go
+++ 07-palindrome.go
@@ -9,14 +9,19 @@ import(
 
 func main(){
 
+  //Declare variables
   var input string
 
+  //Prompt user to enter string and read it from CLI
   fmt.Println("Please enter a string:")
   fmt.Scanf("%s \n", &input)
 
+  //Declare the middle and last value of the string
   middle := len(input)/2
   last := len(input) -1
 
+  //This checks to see if in the loop, the first nd last letters to see if they are not equal
+  //Print if it is or isnt a palindrome 
   for i := 0; i < middle; i++{
     if input[i] != input[last-i]{
       fmt.Printf("%s is not a Palindrome. \n", input)
