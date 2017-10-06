cd443eb7e1e695227a4de98b46b61e7591944222
cwoodlock <g00341460@gmit.ie>
2017-10-06 23:29:46 +0100

    Added append to join lists together and sort to sort them, comments also added

--- 08-merge.go
+++ 08-merge.go
@@ -1,6 +1,9 @@
 //Merge exercise in Go
 //Author: Colm Woodlock
 
+//Adapted from https://stackoverflow.com/questions/16248241/concatenate-two-slices-in-go
+
+
 package main
 
 import(
@@ -17,4 +20,14 @@ func main(){
   //List that will contain the merged list
   list3 := []int{1}
 
+  //Apend the lists together
+  list3 = append(list,list2...)
+
+  //Sort the list3
+  sort.Ints(list3)
+
+  //Print lists and merged and sorted list
+  fmt.Println("List 1:",list)
+  fmt.Println("List 1:",list2)
+  fmt.Println("The merged list is: ",list3)
 }
