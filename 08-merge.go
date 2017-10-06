84ee2baa706e5fdc77b2b6dd1fecaa8e95267791
cwoodlock <g00341460@gmit.ie>
2017-10-06 23:23:46 +0100

    Added lists

---
+++ 08-merge.go
@@ -0,0 +1,20 @@
+//Merge exercise in Go
+//Author: Colm Woodlock
+
+package main
+
+import(
+  "fmt"
+  "sort"
+)
+
+func main(){
+
+  //Declare lists to merge
+  list := []int{1,4,6}
+  list2 := []int{2,3,5}
+
+  //List that will contain the merged list
+  list3 := []int{1}
+
+}
