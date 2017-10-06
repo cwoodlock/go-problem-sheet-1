455c381851835d8d958ff07bfdfaf8d41197d324
cwoodlock <g00341460@gmit.ie>
2017-10-06 22:19:39 +0100

    Show i can generate random number

--- 05-guessinggame.go
+++ 05-guessinggame.go
@@ -3,9 +3,15 @@ package main
 import (
   "fmt"
   "math/rand"
+  "time"
 )
 
 func main(){
 
-  
+  tries := 0
+  previous := 0
+  guess := 0
+  answer := rand.Intn(100)
+
+  fmt.Println(answer)
 }
