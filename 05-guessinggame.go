8043dde20fcbe9a4eb17b13116ce56aeb9bf7433
cwoodlock <g00341460@gmit.ie>
2017-10-06 22:38:18 +0100

    Added comments

--- 05-guessinggame.go
+++ 05-guessinggame.go
@@ -1,3 +1,6 @@
+//Guessing game exercise in Go
+//Author: Colm Woodlock
+
 //Adapted from: https://gobyexample.com/random-numbers
 
 package main
@@ -9,27 +12,30 @@ import (
 
 func main(){
 
+  //Declare variables
   tries := 0
   previous := 0
   guess := 0
   answer := rand.Intn(100)
 
+  //Stay running until the guess is correct
   for guess != answer {
     fmt.Println("Please enter a number between 1 and 100: \n")
     fmt.Scanf("%d", &guess)
 
+    //If guess is correct prompt user
     if guess == answer {
       tries++
       fmt.Printf("Correct! It took you %d times to guess the answer! \n", tries)
-    } else if guess > answer {
+    } else if guess > answer {  //Else if guess is bigger propt user and count it as a try
       fmt.Printf("Your guess was too high please try again! \n")
-      if previous != guess {
+      if previous != guess {  //If guess and the previous guess are the same do not count it as a try
         tries++
         previous = guess
       }
-    } else if guess < answer {
+    } else if guess < answer {  //Else if guess is smaller propt user and count it as a try
       fmt.Printf("Your guess was too low please try again! \n")
-      if previous != guess {
+      if previous != guess {  //If guess and the previous guess are the same do not count it as a try
         tries++
         previous = guess
       }
