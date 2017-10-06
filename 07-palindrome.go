a58651bf958380b85dd2f02d91a519f472c879a3
cwoodlock <g00341460@gmit.ie>
2017-10-06 23:04:47 +0100

    Test enter string

---
+++ 07-palindrome.go
@@ -0,0 +1,20 @@
+//Palindrome exercise in Go
+//Author: Colm Woodlock
+
+package main
+
+import(
+  "fmt"
+)
+
+func main(){
+
+  var input string
+
+  fmt.Println("Please enter a string:")
+  fmt.Scanf("%s \n", &input)
+
+  fmt.Println(input)
+
+
+}
