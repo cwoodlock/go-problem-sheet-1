4e1bc53555dc4ef21b05f67b78da5d7be49d6b34
cwoodlock <g00341460@gmit.ie>
2017-10-06 22:03:56 +0100

    Added comments to code

--- 04-factorial.go
+++ 04-factorial.go
@@ -1,3 +1,6 @@
+//Factorial exercise in Go
+//Author: Colm Woodlock
+
 package main
 
 import "fmt"
@@ -5,12 +8,16 @@ import "fmt"
 //Main function
 func main() {
 
+	//Decalre variables
 	var num int
 	fact := 1
 	sum := 0
+
+	//Prompt user to enter a number and read it in from the CLI
 	fmt.Println("Please enter a number: ")
 	fmt.Scanf("%d", &num)
 
+	//Calculate the factorial of the number they entered check if it is 0
 	if num == 0 {
 		fmt.Printf("%d factorial is 1", num)
 	}
@@ -18,14 +25,18 @@ func main() {
 		fact *= i
 	}
 
+	//Print results
 	fmt.Printf("%d factorial is: %d \n", num, fact)
 	fmt.Printf("The sum of the constituant numbers of %d is: ",fact)
-	
+
+	//The in this loop you get the modulus 10 of the factorial and add it to sum,
+	//Then you divide factorial by 10 and the loop repeats, this gets the sum of the constituant numbers
 	for fact > 0{
 		sum += fact % 10
 		fact /= 10
 	}
 
+	//Print out the sum of the constituant numbers of the factorial
 	fmt.Printf("%d",sum)
 
 
