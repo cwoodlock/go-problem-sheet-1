927e8d3db5b2e932505414f0a380722b5a68032d
cwoodlock <g00341460@gmit.ie>
2017-10-06 22:43:56 +0100

    set up answer

---
+++ 06-largestsmallest.go
@@ -0,0 +1,14 @@
+//Largest and Smallest exercise in Go
+//Author: Colm Woodlock
+
+//Watched parts of this in preparation: https://www.youtube.com/watch?v=bwId7l7aPeg
+
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  
+}
