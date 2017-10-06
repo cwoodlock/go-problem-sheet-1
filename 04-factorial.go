318d956aaf8ad9c2945d7ce84ee8172dcf435dbb
cwoodlock <g00341460@gmit.ie>
2017-10-06 21:51:23 +0100

    add loop to count numbers

--- 04-factorial.go
+++ 04-factorial.go
@@ -7,17 +7,20 @@ func main() {
 
 	var num int
 	fact := 1
+	sum := 0
 	fmt.Println("Please enter a number: ")
 	fmt.Scanf("%d", &num)
 
 	if num == 0 {
 		fmt.Printf("%d factorial is 1", num)
-	} else {
-			for i := num; i > 0; i-- {
-				fact *= i
-			}
 	}
-
+	for i := num; i > 0; i-- {
+		fact *= i
+	}
+	for fact > 0{
+		sum += fact % 10
+		fact /= 10
+	}
 
 
 }
